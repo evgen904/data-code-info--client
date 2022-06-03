a210004407522dbd532e4d06440ff6ff04b533e4
Evgen904 <evgen904@gmail.com>
2022-06-04 01:13:48 +0400

    add dev serve port

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
 
 module.exports = defineConfig({
+  devServer: {
+    port: 80
+  },
   transpileDependencies: true,
   lintOnSave: false,
   chainWebpack: (config) => {
