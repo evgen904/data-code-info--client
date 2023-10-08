99be81e2e5faab74a1c53eae4a17ba7fb9da5978
Evgeniy Dzhurabaev <even904@gmail.com>
2023-10-08 23:54:25 +0400

    Fix MD

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
 const PrerenderSPAPlugin = require('prerender-spa-plugin-next');
+const webpack = require("webpack");
 
 const pluginsWebpack = process.env.NODE_ENV === "production" ? [
     new PrerenderSPAPlugin({
@@ -25,11 +26,16 @@ const pluginsWebpack = process.env.NODE_ENV === "production" ? [
 let remotesCalendar =
   process.env.NODE_ENV === "production"
     ? "https://www.data-code-info.ru/repa1"
-    : "http://localhost:8080";
+    : "http://localhost:8088";
 
 module.exports = defineConfig({
   devServer: {
     port: 5000
+  },
+  pages: {
+    index: {
+      entry: "./src/index.js",
+    },
   },
   transpileDependencies: true,
   lintOnSave: false,
@@ -40,18 +46,23 @@ module.exports = defineConfig({
     );
     config.resolve.alias.set("@", path.resolve(__dirname, "src/"));
     config.resolve.alias.set("assets", path.resolve(__dirname, "src/assets"));
-    config
-      .plugin("module-federation-plugin")
-      .use(require("webpack").container.ModuleFederationPlugin, [
-        {
-          remotes: {
-            calendar: `calendar@${remotesCalendar}/remoteEntry.js`,
-          },
-        },
-      ]);
   },
   configureWebpack: {
-    plugins: pluginsWebpack,
+    plugins: [
+      // ...pluginsWebpack,
+      new webpack.container.ModuleFederationPlugin({
+        name: "app_general",
+        filename: "remoteEntry.js",
+        remotes: {
+          calendar: `calendar@${remotesCalendar}/remoteEntry.js`,
+        },
+        shared: {
+          vue: {
+            singleton: true,
+          },
+        },
+      }),
+    ],
   },
   publicPath:
     process.env.NODE_ENV === "production" ? process.env.VUE_APP_BASEPATH : "/", //"./",
